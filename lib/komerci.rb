0130c45a2fd4ab832f5a9ce68cb5ae50e1b9db2b
Railaine Santos <railaine.santos@alfaconcursos.com.br>
2015-01-27 09:47:35 -0200

    Incluindo CouncilReport

--- lib/komerci.rb
+++ lib/komerci.rb
@@ -2,5 +2,6 @@ require "komerci/version"
 
 module Komerci
   autoload :Transaction, "komerci/transaction"
+  autoload :Transaction, "komerci/council_report"
   autoload :Authorization, "komerci/authorization"
 end
