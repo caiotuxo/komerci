d9352367fd0405d2cd7dbdc72b54f6d954713a92
Caio <caiotuxo@gmail.com>
2015-06-11 17:10:27 -0300

    Update confirm_pre_authorization.rb

--- lib/komerci/confirm_pre_authorization.rb
+++ lib/komerci/confirm_pre_authorization.rb
@@ -9,7 +9,7 @@ module Komerci
     attr_accessor :total, :installments, :date, :filiation_number, :authorization, :confirmation, :user, :password
 
     def send
-      uri = Komerci.uris[self.class.to_s.demodulize.to_s.downcase]
+      uri = URI.parse(Komerci.uris[self.class.to_s.demodulize.to_s.downcase])
       params = {
         :Total    => "%.2f" % total,
         :Parcelas => "%02d" % installments,
